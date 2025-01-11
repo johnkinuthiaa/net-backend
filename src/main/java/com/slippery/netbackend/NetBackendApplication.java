093fe3030095e906db050e182cd55ecb0189056b
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-11 09:44:58 +0300

    initial commit

---
+++ src/main/java/com/slippery/netbackend/NetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.slippery.netbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/slippery/netbackend/NetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slippery.netbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
